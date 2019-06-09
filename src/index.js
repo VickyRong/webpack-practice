84316ca8d2f3cdcd37d938d3d804128dd30c17d5
huyanrong <515020842@qq.com>
2019-06-09 13:02:27 +0800

    基础版管理资源

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
+import './style.css'
+import imgSrc from './pic_01.jpeg'
 
 function component() {
     var element = document.createElement('div');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.innerHTML = _.join(['Hello', 'webpack','杨柳依依'], ' ');
+    element.classList.add('hello');
+    var bgImg = new Image();
+    bgImg.src = imgSrc;
+    element.appendChild(bgImg);
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,28 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules:[
+      {
+        test:/\.css$/,
+        use:[
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test:/\.(png|svg|jpg|jpeg|gif)$/,
+        use:[
+          'file-loader',
+        ]
+      },
+      {
+        test:/\.(woff|woff2|eot|ttf|otf)$/,
+        use:[
+          'file-loader',
+        ]
+      }
+    ]
   }
 };
